797dbb5e34bb70961ae44759a6802cfacddc9b6e
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-11 12:49:43 +0700

    add class user

--- PraktikumRelasidanInheritance_0037.cpp
+++ PraktikumRelasidanInheritance_0037.cpp
@@ -2,3 +2,19 @@
 #include <vector>
 #include <string>
 using namespace std;
+
+class User {
+public:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+    
+    static int generateId() {
+        return ++globalId;
+    }
+    
+    User(string n, string e) : nama(n), email(e), id(generateId()) {}
+};
+
+int User::globalId = 0;
